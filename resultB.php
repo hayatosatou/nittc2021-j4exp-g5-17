d7d251efae1546cb0d5156060fe41b0bc03c1f87
hayatosatou <sh16089@tomakomai.kosen-ac.jp>
2022-02-17 15:01:03 +0900

    connect_to_dbを使用するように変更

--- resultB.php
+++ resultB.php
@@ -3,9 +3,8 @@
 	$max = $_POST['MaxValue'];
 	if(true == empty($min) || true == empty($max))
 		header('location:search.php');
-	$connect = new PDO("pgsql:host=ec2-44-193-188-118.compute-1.amazonaws.com;dbname=d7erkihhtp47qj;port=5432;user=qmrawnpgzlgoaj;password=7f6002961d656ccfd412cc505cf99d5dcbae300f98ce3cf152527d8b90ecc215");
-    	$sql = "SELECT * FROM products WHERE price>=$min AND $max>=price";
-    	$result = $connect->query($sql);
+	$sql = "SELECT * FROM products WHERE price>=$min AND $max>=price";
+    $result = connect_to_db($sql);
 	$row_count = $result->rowCount();
 	foreach($result as $row)
 	{
