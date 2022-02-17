c25d76dc3f9481be1e9d90206a40cdbe4c4ab6ee
hayatosatou <sh16089@tomakomai.kosen-ac.jp>
2022-02-17 15:00:13 +0900

    connect_to_dbを使用するように変更

--- regist.php
+++ regist.php
@@ -3,15 +3,7 @@
   $name = $_SESSION['name'];
   $price = $_SESSION['price'];
   $date = $_SESSION['date'];
-  try{
-    $connect = new PDO("pgsql:host=ec2-44-193-188-118.compute-1.amazonaws.com;dbname=d7erkihhtp47qj;port=5432;user=qmrawnpgzlgoaj;password=7f6002961d656ccfd412cc505cf99d5dcbae300f98ce3cf152527d8b90ecc215");
-    $sql = "INSERT INTO products VALUES('$name','$price','$date')";
-    $result = $connect->query($sql);
-  }
-  catch(PDOException $e)
-  {
-    echo $e->getMessage();
-    die();
-  }
-  header('location:PageA.php');
+  $sql = "INSERT INTO products VALUES('$name','$price','$date')";
+  $result = connect_to_db($sql);
+  header('location:add.php');
 ?>
